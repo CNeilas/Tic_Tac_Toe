5985245670daa833bcd275f9e90f0da905e71b65
CNeilas <humanmusics@gmail.com>
2025-11-04 12:18:47 +0200

    added gameplay boxes

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ import { BoardChecker } from "./modules/boardCheck.js";
 import { Gameboard } from "./modules/gameBoard.js";
 import { positions } from "./modules/positions.js";
 
+function initGame() {
+  const boxCount = 9;
+  const gameTable = document.getElementById("gameTable");
+
+  for (let i = 0; i < boxCount; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+    gameTable.appendChild(div);
+  }
+}
+
 const PlayerX = function (x) {
   let myTurn = true;
   x = "x";
@@ -11,5 +22,7 @@ const PlayerCPU = function (o) {
   let myTurn = false;
   o = "o";
 };
+
+initGame();
 
 // BoardChecker.checkBoard(positions, Gameboard);
